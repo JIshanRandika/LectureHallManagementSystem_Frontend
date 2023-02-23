b77ffa8c14fbcf8cd291a71f6f1cb407b95cc4fe
JIshanRandika <jishanrandika@gmail.com>
2023-02-23 18:30:02 +0530

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,6 +8,7 @@ import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 import LecHallsScreen from '../screens/LecHallsScreen';
 import TimeSlotsScreen from '../screens/TimeSlotsScreen';
+import BookingScreen from '../screens/bookingScreen';
 import {AuthContext} from '../context/AuthContext';
 import SplashScreen from '../screens/SplashScreen';
 
@@ -26,15 +27,110 @@ const Navigation = () => {
             options={{headerShown: false}}
           />
         ) : userInfo.message ? (
-          <Stack.Screen name="Home" component={HomeScreen} />
+          // <Stack.Screen name="Home" component={HomeScreen} />
+            <>
+                <Stack.Screen
+                    name="LecHalls"
+                    options={{
+                        title: 'Lecture Halls',
+                        headerStyle: {
+                            backgroundColor: '#531253',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleAlign:'center',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                    component={LecHallsScreen}
+                />
+                <Stack.Screen
+                    name="Bookings"
+                    options={{
+                        title: 'Booking',
+                        headerStyle: {
+                            backgroundColor: '#531253',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleAlign:'center',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                    component={BookingScreen}
+                />
+                <Stack.Screen
+                    name="TimeSlots"
+                    options={{
+                        title: 'Time Slots',
+                        headerStyle: {
+                            backgroundColor: '#531253',
+                        },
+                        headerTintColor: '#fff',
+                        headerTitleAlign:'center',
+                        headerTitleStyle: {
+                            fontWeight: 'bold',
+                        },
+                    }}
+                    component={TimeSlotsScreen}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Register"
+                    component={RegisterScreen}
+                    options={{headerShown: false}}
+                />
+            </>
+
         ) : (
           <>
               <Stack.Screen
                   name="LecHalls"
+                  options={{
+                      title: 'Lecture Halls',
+                      headerStyle: {
+                          backgroundColor: '#531253',
+                      },
+                      headerTintColor: '#fff',
+                      headerTitleAlign:'center',
+                      headerTitleStyle: {
+                          fontWeight: 'bold',
+                      },
+                  }}
                   component={LecHallsScreen}
               />
               <Stack.Screen
+                  name="Bookings"
+                  options={{
+                      title: 'Booking',
+                      headerStyle: {
+                          backgroundColor: '#531253',
+                      },
+                      headerTintColor: '#fff',
+                      headerTitleAlign:'center',
+                      headerTitleStyle: {
+                          fontWeight: 'bold',
+                      },
+                  }}
+                  component={BookingScreen}
+              />
+              <Stack.Screen
                   name="TimeSlots"
+                  options={{
+                      title: 'Time Slots',
+                      headerStyle: {
+                          backgroundColor: '#531253',
+                      },
+                      headerTintColor: '#fff',
+                      headerTitleAlign:'center',
+                      headerTitleStyle: {
+                          fontWeight: 'bold',
+                      },
+                  }}
                   component={TimeSlotsScreen}
               />
             <Stack.Screen
