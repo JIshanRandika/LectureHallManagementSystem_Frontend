783ff2fd73c44cf84246a5770c68ebd4bf3fcb9f
JIshanRandika <jishanrandika@gmail.com>
2023-02-23 18:29:27 +0530

    update url

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://192.168.8.101:8080/api';
+// export const BASE_URL = 'http://192.168.8.101:8080/api';
+export const BASE_URL = 'https://turquoise-sockeye-fez.cyclic.app/api';
